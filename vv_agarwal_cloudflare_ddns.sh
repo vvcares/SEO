f6e71e65ac7579d3903a9fd3a14541810b86a3be
MAN5 <vvcares@users.noreply.github.com>
2019-02-01 19:51:44 +0800

    Update vv_agarwal_cloudflare_ddns.sh

--- vv_agarwal_cloudflare_ddns.sh
+++ vv_agarwal_cloudflare_ddns.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# wget https://vvcares.com/2/vv_agarwal_cloudflare_ddns.sh -O /sbin/vv_agarwal_cloudflare_ddns.sh
+# wget https://raw.githubusercontent.com/vvcares/others/master/vv_agarwal_cloudflare_ddns.sh -O /sbin/vv_agarwal_cloudflare_ddns.sh
 # chmod +x /sbin/vv_agarwal_cloudflare_ddns.sh
+# Then set Synology DSM task scheduler to run this every 15 minutes (set the Last Run Time as 23:45)
 
-# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN. Your API token is here: https://www.cloudflare.com/a/account/my-account
-#         Make sure the token is the Global token, or has these permissions: #zone:read, #dns_record:read, #dns_records:edit
+# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN. Your Global_API token is here: https://www.cloudflare.com/a/account/my-account
+#         Make sure or has these permissions: #zone:read, #dns_record:read, #dns_records:edit
 # Step 2: Create an A record on Cloudflare with the subdomain you chose
-# Step 3: Run "./ddns.sh -l" to get the zone_id and rec_id of the record you created.
+# Step 3: Run "/sbin/vv_agarwal_cloudflare_ddns.sh -l" to get the zone_id and rec_id of the record you created.
 #         Fill in ZONE_ID and REC_ID below
 #         This step is optional, but will save you 2 requests every time you this script
-# Step 4: Run "./ddns.sh". It should tell you that record was updated or that it didn't need updating.
+# Step 4: Run "/sbin/vv_agarwal_cloudflare_ddns.sh". It should tell you that record was updated or that it didn't need updating.
 # Step 5: Run it every hour with cron. Use the '-s' flag to silence normal output
 #         0 * * * * /path/to/ddns.sh -s
 
@@ -54,8 +55,5 @@ if [ "$IP" == "$RECORD_IP" ]; then
 fi
 
 $VERBOSE && echo "Setting IP to $IP"
-
 $CURL -X PUT "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" --data '{"type":"A","name":"'"$SUBDOMAIN"'","content":"'"$IP"'","proxied":false,"ttl":300}' 1>/dev/null
-
-
 exit 0
