9e7b93dad6c83ce56dd839b0667762f956d6eb91
MAN5 <vvcares@users.noreply.github.com>
2021-09-12 08:36:59 +0530

    Update vv_cloudflare_ddns.sh

--- vv_cloudflare_ddns.sh
+++ vv_cloudflare_ddns.sh
@@ -1,28 +1,36 @@
 #!/usr/bin/env bash
+# wget https://raw.githubusercontent.com/vvcares/test/master/vvdns.sh -O /sbin/vvdns.sh.sh
+# chmod +x /sbin/vv_agarwal_cloudflare_ddns.sh
+# Step 1: Fill in EMAIL, TOKEN, DOMAIN and SUBDOMAIN. Your API token is here: https://www.cloudflare.com/a/account/my-account
+#         Make sure the token is the Global token, or has these permissions: #zone:read, #dns_record:read, #dns_records:edit
+# Step 2: Create an A record on Cloudflare with the subdomain you chose
+# Step 3: Run "./ddns.sh -l" to get the zone_id and rec_id of the record you created.
+#         Fill in ZONE_ID and REC_ID below
+#         This step is optional, but will save you 2 requests every time you this script
+# Step 4: Run "./ddns.sh". It should tell you that record was updated or that it didn't need updating.
+# Step 5: Run it every hour with cron. Use the '-s' flag to silence normal output
+#         0 * * * * /path/to/ddns.sh -s
 
-# This script will lookup the ZONE_ID/REC_ID itself automatically. If you could provide, the process will be much faster.
-# STEP1: Create an A record on Cloudflare with the subdomain you chose. Your TOKEN/Global_API is here: https://www.cloudflare.com/a/account/my-account
-# STEP2: wget https://raw.githubusercontent.com/vvcares/others/master/vv_cfddns.sh -O /vv_files/vv_cfddns.sh
-# STEP3: chmod +x /vv_files/vv_cfddns.sh
-# STEP4: RUN this file with task scheduler with parameters as below..
-# STEP5: bash vv_cfddns.sh -s EMAIL GLOBAL_API SUB_DOMAIN ROOT_DOMAIN_FQDN CF_PROXY_TRUE_FALSE
+#*** IF YOU WANT TO USE DROPLET'S MAIN IP USE THIS BELOW LINE
+#IP="$(curl -s http://ipv4.icanhazip.com)"
+#*** IF YOU WANT TO USE DROPLET'S FLOATING IP USE THIS BELOW LINE
+#IP="$(curl --interface "$ANCHOR_IP" https://api.ipify.org/)"
 
-# example : bash /vv_files/vv_cfddns.sh -s EMAILID GLOBAL_API SUB_DOMAIN MAIN_DOMAIN PROXY_true_false
-# IF JUST WANT TO GET Cloudflare RECORD_ID ?: bash vv_cfddns.sh -l EMAIL Global_API SUB_domain ROOT_DOMAIN ZONE_ID
+EMAIL='mymail@mymail.comx'
+#Global API
+TOKEN='CFs Global API TOKEN'
+DOMAIN='domain.com'
+SUBDOMAIN='vpn'
+ZONE_ID='CFs domain zone id'
+REC_ID=''
 
-EMAIL=$2
-TOKEN=$3
-SUBDOMAIN=$4
-DOMAIN=$5
-PROXIED=$6
-ZONE_ID='' #This script will get it automatically
-REC_ID='' #This script will get it automatically
+ANCHOR_IP="$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address)"
+IP="$(curl --interface "$ANCHOR_IP" https://api.ipify.org/)" 
+
 set -euo pipefail
-set -x # enable for debugging
-
-VERBOSE=" [ '${1:-}' != '-s' ] "
-LOOKUP=" [ '${1:-}' == '-l' ] "
-
+#set -x # enable for debugging
+VERBOSE="[ '${1:-}' != '-s' ]"
+LOOKUP="[ '${1:-}' == '-l' ]"
 API_URL="https://api.cloudflare.com/client/v4"
 CURL="curl -s \
   -H Content-Type:application/json \
@@ -41,16 +49,12 @@ fi
 
 $LOOKUP && exit 0
 
-IP="$(curl -s http://ipv4.icanhazip.com)"
 RECORD_IP="$($CURL "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" | sed -e 's/[{}]/\n/g' | sed -e 's/,/\n/g' | grep '"content":"' | cut -d'"' -f4)"
-
-if [ "$IP" = "$RECORD_IP" ]; then
+if [ "$IP" == "$RECORD_IP" ]; then
   $VERBOSE && echo "IP Unchanged"
   exit 0
 fi
 
 $VERBOSE && echo "Setting IP to $IP"
-
-$CURL -X PUT "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" --data '{"type":"A","name":"'"$SUBDOMAIN"'","content":"'"$IP"'","proxied":'$PROXIED'}' 1>/dev/null
-
+$CURL -X PUT "$API_URL/zones/$ZONE_ID/dns_records/$REC_ID" --data '{"type":"A","name":"'"$SUBDOMAIN"'","content":"'"$IP"'","proxied":false,"ttl":300}' 1>/dev/null
 exit 0
